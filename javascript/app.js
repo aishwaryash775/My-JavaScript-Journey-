d2537aee5731f630b954c4e5510b9b032e6c36b5
AISHWARYA SANTOSH SHINDE <aishwaryash880@gmail.com>
2025-09-13 22:16:41 +0530

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+for (let i=1; i<=5;i++) {
+    document.writeln(i);
+    
+}
+//odd
+for (i=1; i<=15 ;i=i+2){
+    document.writeln(i);
+}
+
+//even
+for (i=2; i<=10 ;i=i+2){
+    document.writeln(i);
+}
+let n= prompt("write your number");
+n = parseInt(n);
+
+for(let i = n ; i<=n*10 ; i=i+n){
+  document.writeln(i);
+
+}
+//multiplication
+
+for (i=5; i<=50 ;i=i+5){
+    document.writeln(i);
+}
