e1cc95eea1ff4ea0f35f3a7baa0d8678a1dccef2
AISHWARYA SANTOSH SHINDE <aishwaryash880@gmail.com>
2025-09-14 16:06:09 +0530

    Add files via upload

---
+++ javascript/objectliteral.js
@@ -0,0 +1,23 @@
+const post = {
+    username : "@aishwaryashinde",
+      content : "This  isnmy #firstpost",
+      like : 179,
+      reposts : 19,     
+      tags : ["@shindehouse","@shinde"]
+
+};
+
+const classinfo ={
+     Aish : {
+        grade : "A+",
+        city : "mumbai"
+     },
+     Prem : {
+        grade : "B+",
+        city : "mumbai"
+     },
+     Divya : {
+        grade : "C+",
+        city : "pakistan"
+     }
+};
