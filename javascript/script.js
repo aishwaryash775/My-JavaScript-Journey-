cba8eef61f3c0fb6c32f9c311dc401970bd2552c
AISHWARYA SANTOSH SHINDE <aishwaryash880@gmail.com>
2025-09-16 14:55:44 +0530

    object literal script.js

--- javascript/script.js
+++ javascript/script.js
@@ -42,3 +42,61 @@ let id = setInterval(() => {
 
 document.writeln(id);
 
+
+
+document.writeln(id ); 
+ 
+let arr = [1,3,4,5,6,7,8,3,7];
+
+
+
+let max = arr.reduce((max,el) =>{
+    if (max < el) {
+       
+        return el;
+    } else {
+        return max;
+    }
+});
+document.writeln(max);
+
+
+
+
+
+function sum(a,b =3){
+    return  a+b;
+
+
+}
+sum(2);
+
+
+let arr = [1,3,4,5,6,8,9,2];
+let newArr =  [...arr];
+
+let chars= [..."hello"];
+let even =[2,4,6,8,10];
+let odd = [1,3,5,7,9];
+
+
+let nums =[...even ,...odd];
+
+
+/spread with object literal 
+
+const data= {
+    email :"aish123@gmail.com",
+    password: "abcd",
+};
+
+const datacopy = {...data , id:123 , country:"india"};
+   
+
+
+function sum(...args) { 
+    return args.reduce((sum, el) => sum + el, 0);
+}
+
+
+
